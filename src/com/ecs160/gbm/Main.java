84a364ce120312ab33a1ed2fbb08433a1b888a13
Kevin Jacobs <stealtherx@gmail.com>
2011-04-12 11:09:08 -0700

    I found a tutorial on sending and receiving SMS messages and implemented it into the code.

--- src/com/ecs160/gbm/Main.java
+++ src/com/ecs160/gbm/Main.java
@@ -1,7 +1,10 @@
 package com.ecs160.gbm;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main extends Activity {
     /** Called when the activity is first created. */
@@ -10,6 +13,20 @@ public class Main extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        
+        Button sendMess = (Button) findViewById(R.id.sendMessage);
+        sendMess.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				sendMessage();
+			}
+		});
+		
+
+    }
+	
+    void sendMessage(){
+    	Intent i = new Intent(this, SendMessage.class);
+    	startActivity(i);    	
     }
 }
